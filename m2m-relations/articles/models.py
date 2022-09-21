79ab2c1dae309c0ed762e464530bcb8fd3aa8c49
Dmitriy Khusainov <evilsonk@gmail.com>
2022-09-21 20:36:30 +0500

    final commit plz no buggerino

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -25,12 +25,15 @@ class Tag(models.Model):
     class Meta:
         verbose_name = 'Тэг'
         verbose_name_plural = 'Тэги'
+    
+    def __str__(self):
+        return self.name
 
 
 class Scope(models.Model):
 
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scope')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scope')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
     is_main = models.BooleanField()
 
     class Meta:
